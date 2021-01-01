4b6f46336259c2c2fdcb341e06674d8d6c4d4d20
my pc <avnees.bana002@gmail.com>
2021-01-02 00:42:55 +0530

    avi 12:42

--- MyJavaPrograms/src/MyPackage/FirstJava.java
+++ MyJavaPrograms/src/MyPackage/FirstJava.java
@@ -4,10 +4,10 @@ public class FirstJava {
 	public static void main(String[] args) {
 		
 
-
-		System.out.println("My Area is this");
-		System.out.println("My Area is this 123");
-		
+		System.out.println("My Area is this 1");
+		System.out.println("My Area is this 2");
+		System.out.println("My Area is this 3");
+		System.out.println("My Area is this 4");
 
 
 
